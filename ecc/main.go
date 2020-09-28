6112b3bd1db9f3a8d24b3ee1fb0273daf2f3ec25
Hongbin Mao <hello2mao@gmail.com>
2020-09-28 09:01:46 +0800

    Lazy push: Mon Sep 28 09:01:46 CST 2020

--- ecc/main.go
+++ ecc/main.go
@@ -4,10 +4,101 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
-	"encoding/hex"
+	"errors"
 	"fmt"
+	"math/big"
 	"os"
 )
+
+const (
+	// number of bits in a big.Word
+	wordBits = 32 << (uint64(^big.Word(0)) >> 63)
+	// number of bytes in a big.Word
+	wordBytes = wordBits / 8
+)
+
+// ReadBits encodes the absolute value of bigint as big-endian bytes. Callers must ensure
+// that buf has enough space. If buf is too short the result will be incomplete.
+func ReadBits(bigint *big.Int, buf []byte) {
+	i := len(buf)
+	for _, d := range bigint.Bits() {
+		for j := 0; j < wordBytes && i > 0; j++ {
+			i--
+			buf[i] = byte(d)
+			d >>= 8
+		}
+	}
+}
+
+// PaddedBigBytes encodes a big integer as a big-endian byte slice. The length
+// of the slice is at least n bytes.
+func PaddedBigBytes(bigint *big.Int, n int) []byte {
+	if bigint.BitLen()/8 >= n {
+		return bigint.Bytes()
+	}
+	ret := make([]byte, n)
+	ReadBits(bigint, ret)
+	return ret
+}
+
+// 私钥 -> []byte
+// FromECDSA exports a private key into a binary dump.
+func FromECDSA(priv *ecdsa.PrivateKey) []byte {
+	if priv == nil {
+		return nil
+	}
+	return PaddedBigBytes(priv.D, priv.Params().BitSize/8)
+}
+
+// []byte -> 私钥
+// ToECDSA creates a private key with the given D value.
+func ToECDSA(d []byte) (*ecdsa.PrivateKey, error) {
+	return toECDSA(d, true)
+}
+
+// toECDSA creates a private key with the given D value. The strict parameter
+// controls whether the key's length should be enforced at the curve size or
+// it can also accept legacy encodings (0 prefixes).
+func toECDSA(d []byte, strict bool) (*ecdsa.PrivateKey, error) {
+	priv := new(ecdsa.PrivateKey)
+	priv.PublicKey.Curve = elliptic.P256()
+	if strict && 8*len(d) != priv.Params().BitSize {
+		return nil, fmt.Errorf("invalid length, need %d bits", priv.Params().BitSize)
+	}
+	priv.D = new(big.Int).SetBytes(d)
+
+	// The priv.D must < N
+	if priv.D.Cmp(elliptic.P256().Params().N) >= 0 {
+		return nil, fmt.Errorf("invalid private key, >=N")
+	}
+	// The priv.D must not be zero or negative.
+	if priv.D.Sign() <= 0 {
+		return nil, fmt.Errorf("invalid private key, zero or negative")
+	}
+
+	priv.PublicKey.X, priv.PublicKey.Y = priv.PublicKey.Curve.ScalarBaseMult(d)
+	if priv.PublicKey.X == nil {
+		return nil, errors.New("invalid private key")
+	}
+	return priv, nil
+}
+
+// 公钥 -> []byte
+func FromECDSAPub(pub *ecdsa.PublicKey) []byte {
+	if pub == nil || pub.X == nil || pub.Y == nil {
+		return nil
+	}
+	return elliptic.Marshal(elliptic.P256(), pub.X, pub.Y)
+}
+
+// []byte -> 公钥
+func ToECDSAPub(pub []byte) *ecdsa.PublicKey {
+	if len(pub) == 0 {
+		return nil
+	}
+	x, y := elliptic.Unmarshal(elliptic.P256(), pub)
+	return &ecdsa.PublicKey{Curve: elliptic.P256(), X: x, Y: y}
+}
 
 func main() {
 
@@ -21,12 +112,10 @@ func main() {
 		os.Exit(-1)
 	}
 	fmt.Printf("PrivKey.D: %v\n", privKey.D)
+	fmt.Printf("PrivKey bytes: %x\n", FromECDSA(privKey))
 	fmt.Printf("PublicKey.X: %v\n", privKey.X)
 	fmt.Printf("PublicKey.Y: %v\n", privKey.Y)
-
-	// Marshall the public key
-	marshalledPubKey := elliptic.Marshal(p256, privKey.X, privKey.Y)
-	fmt.Printf("marshalledPubKey: %v\n", hex.EncodeToString(marshalledPubKey))
+	fmt.Printf("PublicKey bytes: %x\n", FromECDSAPub(&privKey.PublicKey))
 
 	// Marshall the public key
 	// go version >= 1.15
