148757493773a8cf72a46c67ebbac1bf1c5da5d9
Hongbin Mao <hello2mao@gmail.com>
2020-10-28 11:17:34 +0800

    Lazy push: Wed Oct 28 11:17:34 CST 2020

--- Secret-Sharing/main.go
+++ Secret-Sharing/main.go
@@ -12,6 +12,8 @@ func main() {
 	secret := "0y10VAfmyH7GLQY6QccCSLKJi8iFgpcSBTLyYOGbiYPqOpStAf1OYuzEBzZR"
 	w := 5
 	t := 3
+
+	// 分割秘密
 	secretShares, err := sssa.Create(t, w, secret)
 	if err != nil {
 		fmt.Printf("Create err: %v\n", err)
@@ -19,11 +21,13 @@ func main() {
 	}
 	fmt.Printf("secretShares: %v\n", secretShares)
 
+	// 选择其中的3份
 	testShares := []string{
 		secretShares[0],
 		secretShares[1],
 		secretShares[2],
 	}
+	// 恢复秘密
 	combined, err := sssa.Combine(testShares)
 	if err != nil {
 		fmt.Printf("Combine err: %v\n", err)
